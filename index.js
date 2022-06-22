887253e001fd77c1741d15d58b5f24d95d0864b0
sakaitakumi <sakataku.0908@icloud.com>
2022-06-22 10:51:26 +0900

    chj

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-// TODO: 公式リファレンスに沿って JavaScript を記述
-const TwoWayBinding = {
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-}
-  
-Vue.createApp(TwoWayBinding).mount('#two-way-binding')
-  
-const ConditionalRendering = {
-    data() {
-      return {
-        seen: true
-      }
-    }
-}
-  
-Vue.createApp(ConditionalRendering).mount('#conditional-rendering')
+const RootComponent = {};
 
-const ListRendering = {
-    data() {
-      return {
-        todos: [
-          { text: 'Learn JavaScript' },
-          { text: 'Learn Vue' },
-          { text: 'Build something awesome' }
-        ]
-      }
-    }
-}
-  
-Vue.createApp(ListRendering).mount('#list-rendering')
+Vue.createApp(RootComponent).mount('#app');
 
 
