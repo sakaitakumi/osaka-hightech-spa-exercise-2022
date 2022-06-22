ecd24a2627a63ff57b77981555d988c5b867fb9d
sakaitakumi <sakataku.0908@icloud.com>
2022-06-22 10:16:01 +0900

    ij2

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 // TODO: 公式リファレンスに沿って JavaScript を記述
-const EventHandling = {
-    data() {
-      return {
-        message: 'Hello Vue.js!'
-      }
-    },
-    methods: {
-      reverseMessage() {
-        this.message = this.message
-          .split('')
-          .reverse()
-          .join('')
-      }
-    }
-}
-  
 Vue.createApp(EventHandling).mount('#event-handling')
 
 const TwoWayBinding = {
@@ -50,3 +34,5 @@ const ListRendering = {
 }
   
 Vue.createApp(ListRendering).mount('#list-rendering')
+
+
