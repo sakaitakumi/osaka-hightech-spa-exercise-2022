35946549d2e107dd63fc58909e00d7c9699313bc
sakaitakumi <sakataku.0908@icloud.com>
2022-06-22 10:03:57 +0900

    ij

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 // TODO: 公式リファレンスに沿って JavaScript を記述
-const AttributeBinding = {
-    data() {
-      return {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-      }
-    }
-}
-  
-Vue.createApp(AttributeBinding).mount('#bind-attribute')
-
 const EventHandling = {
     data() {
       return {
