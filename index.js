8c41209f4bb764f25bfede95d82093d9ba928435
sakaitakumi <sakataku.0908@icloud.com>
2022-07-06 11:56:30 +0900

    q

--- index.js
+++ index.js
@@ -65,8 +65,7 @@ const PopulationBarPlot = {
   props: [ 'api' ],
   data() {
     return {
-      populations: [ 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 ],
-      result: '',
+      populations: [],
     };
   },
   /* html */
