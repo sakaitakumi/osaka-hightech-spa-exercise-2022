ae5f6be76328cf6516a5cd55f81fa1caf8d8fe07
sakaitakumi <sakataku.0908@icloud.com>
2022-07-06 11:25:30 +0900

    f

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ const PopulationBarPlot = {
     async updateGraph() {
       let foo = await getPopulations(this.api, 27,);
 
-      foo = foo['result']['data']['label:総人口'];
+      foo = foo['result']['data'][0];
 
       this.result = foo;
     },
